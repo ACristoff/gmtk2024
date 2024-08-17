20f3e11f930d8492d1e75a7156ff89f77314f25b
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-16 19:23:33 -0700

    merge with latest

--- gmtk2024/scenes/game/characters/wormo/wormo.gd
+++ gmtk2024/scenes/game/characters/wormo/wormo.gd
@@ -3,34 +3,37 @@ class_name Player
 
 @export var speed := 125.0
 @export var gravity := 400
-@export var jump_force := 200
+@export var jump_force := 100
 
 @export var coyote_time = 0.3
 var coyote_timer = 0.3
 
 var active = true
+var is_charging = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 ##TODO SFX
 
-func jump(force):
+func jump_release(force):
 	velocity.y = -force
-	coyote_timer = 0
+	#coyote_timer = 0
 
 func _physics_process(delta):
 	if is_on_floor() == false:
 		velocity.y += gravity * delta
-		coyote_timer -= 1 * delta
+		#coyote_timer -= 1 * delta
 	if velocity.y > 500:
 		velocity.y = 500
 	var direction = 0
 	velocity.x = direction * speed
 	if active == true:
-		if Input.is_action_just_pressed("jump") && coyote_timer > 0:
-			jump(jump_force)
+		if Input.is_action_just_released("jump"):
+			jump_release(jump_force)
+			jump_force = 100
 			pass
-		if is_on_floor_only() && !Input.is_action_just_pressed("jump"):
-			coyote_timer = coyote_time
+		if Input.is_action_pressed("jump"):
+			jump_force += 10
+			pass
 		direction = Input.get_axis("move_left", "move_right")
 		if direction != 0:
 			animated_sprite.flip_h = (direction == -1)
