fd46cd07345c4906a33a51192c47bcaadedf57d5
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-16 18:58:59 -0700

    coyote timer fix

--- gmtk2024/scenes/game/characters/wormo/wormo.gd
+++ gmtk2024/scenes/game/characters/wormo/wormo.gd
@@ -1,5 +1,58 @@
 extends CharacterBody2D
+class_name Player
 
+@export var speed := 125.0
+@export var gravity := 400
+@export var jump_force := 200
+
+@export var coyote_time = 0.3
+var coyote_timer = 0.3
+
+var active = true
+
+@onready var animated_sprite = $AnimatedSprite2D
+##TODO SFX
+
+func jump(force):
+	velocity.y = -force
+	coyote_timer = 0
+
+func _physics_process(delta):
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		coyote_timer -= 1 * delta
+	if velocity.y > 500:
+		velocity.y = 500
+	var direction = 0
+	velocity.x = direction * speed
+	if active == true:
+		if Input.is_action_just_pressed("jump") && coyote_timer > 0:
+			jump(jump_force)
+			pass
+		if is_on_floor_only() && !Input.is_action_just_pressed("jump"):
+			coyote_timer = 0.3
+		direction = Input.get_axis("move_left", "move_right")
+		if direction != 0:
+			animated_sprite.flip_h = (direction == -1)
+	velocity.x = direction * speed
+	move_and_slide()
+	update_animations(direction)
+	pass
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+		pass
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+		pass
+	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -7,5 +60,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
