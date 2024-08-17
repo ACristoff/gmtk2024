5e5407f9f5eab14ba743c0ff76087197247fce00
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-16 19:34:47 -0700

    charged jump wip

--- gmtk2024/scenes/game/characters/wormo/wormo.gd
+++ gmtk2024/scenes/game/characters/wormo/wormo.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 class_name Player
 
 @export var speed := 125.0
-@export var gravity := 400
+@export var gravity := 500
 @export var jump_force := 100
 
 @export var coyote_time = 0.3
+@export var jump_acceleration = 5
 var coyote_timer = 0.3
 
 var active = true
@@ -29,10 +30,12 @@ func _physics_process(delta):
 	if active == true:
 		if Input.is_action_just_released("jump"):
 			jump_release(jump_force)
+			is_charging = false
 			jump_force = 100
 			pass
 		if Input.is_action_pressed("jump"):
-			jump_force += 10
+			is_charging = true
+			jump_force += jump_acceleration
 			pass
 		direction = Input.get_axis("move_left", "move_right")
 		if direction != 0:
@@ -43,9 +46,14 @@ func _physics_process(delta):
 	pass
 
 func update_animations(direction):
+		#if is_charging == true:
+		#animated_sprite.play("charge")
 	if is_on_floor():
 		if direction == 0:
-			animated_sprite.play("idle")
+			if is_charging == true && animated_sprite.animation != "charge":
+				animated_sprite.play("charge")
+			elif is_charging == false:
+				animated_sprite.play("idle")
 		else:
 			animated_sprite.play("run")
 		pass
