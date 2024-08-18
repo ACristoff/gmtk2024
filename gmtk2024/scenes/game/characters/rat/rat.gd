9f6a62e6c4f71a9c04edaab6b0937db783697351
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-17 23:12:30 -0700

    killing bites

--- gmtk2024/scenes/game/characters/rat/rat.gd
+++ gmtk2024/scenes/game/characters/rat/rat.gd
@@ -2,7 +2,11 @@ extends CharacterBody2D
 
 @onready var _animation_player = $AnimationPlayer
 @onready var animated_sprite = $Sprite2D
+@onready var bite_box = $BiteBox/CollisionShape2D
 
+func _ready():
+	#bite_box.set_process(false)
+	bite_box.disabled = true
 
 func _input(_event):
 	if Input.is_action_just_pressed("attack"):
@@ -11,11 +15,18 @@ func _input(_event):
 	pass
 
 
+func bite():
+	print("KILLING BITES")
+	bite_box.disabled = false
+	#bite_box.set_process(true)
 	#if Input.is_action_just_pressed("attack"):
 		#print("attack!")
 		#actor._animation_player.play("attack")
 
+func finish_bite():
+	bite_box.disabled = true
+	pass
 
 func _on_animation_player_animation_finished(anim_name):
-	print(anim_name)
+	#print(anim_name)
 	pass # Replace with function body.
