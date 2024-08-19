119826b3241d92ceeb9126851636681ce5950b36
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-19 04:04:57 -0700

    exit point

--- gmtk2024/scenes/_debug/level_base.gd
+++ gmtk2024/scenes/_debug/level_base.gd
@@ -8,23 +8,31 @@ var timer_node = null
 var time_left = null
 var winCon = false
 @onready var start = $Start
-@onready var exit = null
+@onready var exit = $Exit
 @onready var death_zone = $Deathzone
 @onready var hurt_sfx = null
 
 @onready var hud = null
+
 
 func _ready():
 	player = get_tree().get_first_node_in_group("player")
 	if player != null:
 		player.global_position = start.get_spawn_pos()
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	exit.body_entered.connect(_on_exit_body_entered)
 
 func _on_deathzone_body_entered(_body):
 	#print("oopsy woopsy!")
 	reset_player()
 	pass
 
+
+
 func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	prints("exit reached", body)
+	pass
