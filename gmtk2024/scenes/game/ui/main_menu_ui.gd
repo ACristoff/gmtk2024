439122bb88b729d43609c844306ce66b9552ed26
Alex Cristoffanini <acristoffanini@gmail.com>
2024-08-20 00:30:13 -0700

    controls fix

--- gmtk2024/scenes/game/ui/main_menu_ui.gd
+++ gmtk2024/scenes/game/ui/main_menu_ui.gd
@@ -3,6 +3,7 @@ extends Control
 @export var level_theme: AudioStreamMP3 = null
 @export var next_level: PackedScene = null
 
+@onready var instructions_panel = $PanelContainer 
 
 func _ready():
 	if level_theme != null:
@@ -16,7 +17,8 @@ func _on_start_game_pressed():
 
 
 func _on_controls_pressed():
-	pass
+	instructions_panel.visible = !instructions_panel.visible
+	pass # Replace with function body.
 
 
 func _on_exit_pressed():
